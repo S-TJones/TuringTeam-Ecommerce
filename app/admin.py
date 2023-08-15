984ee0979d759f5dc91f4a85a5e5996a8838b9fa
r11Sr <rs_burner2@outlook.com>
2023-08-14 21:10:31 -0500

    started to add endpoints as stub methods for administrator Services

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,36 @@
 from flask import Blueprint, jsonify, current_app, session
+from flask_login import login_required
 
 admin = Blueprint('admin', __name__, url_prefix='/api/v1/admin')
 
 @admin.route('/status')
+@login_required
 def status():
     return jsonify({ "message": "API Operational for admin" })
+
+@admin.route('/users',method=['GET'])
+@login_required
+def view_users():
+    pass
+
+@admin.route('/users/<userID>',methods=['GET','POST'])
+@login_required
+def user_details(userID):
+    pass
+
+@admin.route('/product/',methods=['GET','POST'])
+@login_required
+def createProduct():
+    pass
+
+
+@admin.route('/product/<productID>',methods=['GET','POST'])
+@login_required
+def updateProductDetails(product):
+    pass
+
+
+@admin.route('/product/<productID>',method = ['DELETE'])
+@login_required
+def deleteProduct(productID):
+    pass
