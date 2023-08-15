861df8c9cb87cefba118ffcfb5bbc44a000e12b7
r11Sr <rs_burner2@outlook.com>
2023-08-14 20:35:40 -0500

    for init file added login_manager and admin + customer blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,27 @@ from flask import Flask
 from .config import Config
 from .extensions import db, migrate
 from .main import main
-from .api import api
+from .admin import admin
+from .customer import customer
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    # db = SQLAlchemy(app)
     # db.init_app(app)
     # migrate.init_app(db, app)
 
-    app.register_blueprint(main)
-    app.register_blueprint(api)
+    # Flask-Login login manager
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'main.login'
+    login_manager.blueprint_login_views = {
+    'main' : '/login'
+    }
+
+
+    app.register_blueprint(admin)
+    app.register_blueprint(customer)
 
     return app
--- app/main.py
+++ app/main.py
@@ -10,3 +10,16 @@ def index():
 @main.route('/about')
 def about():
     return render_template('about.html')
+
+@main.route("/login", methods=["GET", "POST"])
+def login():
+    pass
+
+@main.route('/logout')
+def logout():
+        """Logout of the application"""
+        logout_user() # from Flask-login
+        session.clear() 
+        flash('You were logged out', 'success')
+        return redirect(url_for('main.index'))
+
