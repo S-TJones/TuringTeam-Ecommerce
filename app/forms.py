406a4953e38406a3ab1edd19e7543e8ba569ac6a
S-TJones <jones.shemar.sj@gmail.com>
2023-08-14 20:59:16 -0500

    Added the Forms file
    This is a draft file for the forms. This may be subjected to change in the future.

---
+++ app/forms.py
@@ -0,0 +1,32 @@
+# from itertools import product
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField,TextAreaField, SelectField
+from wtforms.validators import InputRequired, DataRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+class RegistrationForm(FlaskForm):
+    firstName = StringField('First Name', validators=[InputRequired()])
+    lastName = StringField('Last Name', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired()])
+    password = StringField('Password', validators=[InputRequired()])
+    retypePassword = StringField('Re-enter Password', validators=[InputRequired()])
+
+#----------------------------------------------------------------------
+
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+
+#----------------------------------------------------------------------
+
+class ProductForm(FlaskForm):
+    name = StringField('Name', validators=[InputRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    price= StringField('Price', validators=[InputRequired()])
+    image = FileField('Image',validators=[FileRequired(),FileAllowed(['jpg','png','jpeg'],'Select image files only.')])
+    
+#----------------------------------------------------------------------
+
+class UpdateOrder(FlaskForm):
+    status_options= SelectField('status', validators=[DataRequired()])
+
